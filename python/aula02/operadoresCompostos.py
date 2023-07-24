e07b9d46c2bd0c106b45ee0dcf677af7d8d5e5cb
Vinicius Vaz <agvazvinicius@gmail.com>
2023-07-24 16:15:00 -0300

    operadores compostos

---
+++ python/aula02/operadoresCompostos.py
@@ -0,0 +1,22 @@
+x=10 
+print(f'- x = {x}')
+
+# Adição:
+x+=2
+print(f'- x = {x}')
+
+# Subtração:
+x-=2
+print(f'- x = {x}')
+
+# Multiplicação:
+x*=2 
+print(f'- x = {x}')
+
+# Divição:
+x/=2
+print(f'- x = {x}')
+
+# Módulo:
+x%=2
+print(f'- x = {x}')
