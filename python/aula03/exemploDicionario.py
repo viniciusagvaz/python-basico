9f5136e3fdf66b23fd358d4926b6ad17fc8b4b07
Vinicius Vaz <agvazvinicius@gmail.com>
2023-07-24 17:04:28 -0300

    listaSequencial

---
+++ python/aula03/exemploDicionario.py
@@ -0,0 +1,6 @@
+pessoas = {1111:['Alberto'], 2222:['Bruna'], 3333:['Cecília'], 4444:['Daniel']}
+
+print(f"primeira pessoa = {pessoas[1111]}")
+print(f"segunda pessoa = {pessoas[2222]}")
+print(f"terceira pessoa = {pessoas[3333]}")
+print(f"quarta pessoa = {pessoas[4444]}")
---
+++ python/aula03/string.py
@@ -0,0 +1,4 @@
+nome = 'exemplo de string em Python'
+print(nome)
+print('1 - o valor da variável "nome" é: {}'.format(nome))
+print(f'2 - o valor da variável "nome" é: {nome}')
